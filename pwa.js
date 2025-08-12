cb4720fe15ee52b35569228a436d533435f98a58
KostyaLKE <kostyalitvinov8@gmail.com>
2025-08-12 15:07:21 +0300

    Create pwa.js

---
+++ pwa.js
@@ -0,0 +1,8 @@
+// Регистрация Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js')
+      .then(reg => console.log('✅ Service Worker зарегистрирован:', reg.scope))
+      .catch(err => console.warn('❌ Ошибка регистрации SW:', err));
+  });
+}
